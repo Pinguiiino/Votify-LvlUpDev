81f203fdc41969e97743d82a37a9daf8c21293e7
Javier Cabañero Atienzar <JCABATI@upv.edu.es>
2026-03-04 16:45:49 +0100

    Creados datos de prueba

--- src/Votify.Api/Program.cs
+++ src/Votify.Api/Program.cs
@@ -24,4 +24,20 @@ app.UseCors("AllowBlazor");
 
 app.MapGet("/", () => "API funcionando");
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<VotifyDbContext>();
+
+        await Votify.Infrastructure.Data.DbSeeder.SeedAsync(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocurrió un error al poblar la base de datos.");
+    }
+}
+
 app.Run();
---
+++ src/Votify.Infrastructure/DbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Votify.Domain;
+using Votify.Domain.UserFolder;
+using Votify.Domain.VoteFoler;
+
+namespace Votify.Infrastructure.Data;
+
+public static class DbSeeder
+{
+    public static async Task SeedAsync(VotifyDbContext context)
+    {
+        await context.Database.MigrateAsync();
+
+        if (await context.Usuarios.AnyAsync() || await context.Votes.AnyAsync())
+        {
+            return;
+        }
+        var jurado1 = new Jury("Ada Lovelace", "ada@test.com", "Contraseña");
+        var publico1 = new Public("Alan Turing", "alan@test.com", "Contraseña");
+
+        context.Usuarios.AddRange(jurado1, publico1);
+        await context.SaveChangesAsync();
+
+        var idProyectoFicticio = Guid.NewGuid().ToString();
+
+        var votoExperto = new ExpertVote(
+            idProyectoFicticio,
+            jurado1.Id.ToString(),
+            5 
+        );
+
+        var votoPublico = new PublicVote(
+            idProyectoFicticio,
+            publico1.Id.ToString()
+        );
+
+        context.Votes.AddRange(votoExperto, votoPublico);
+        await context.SaveChangesAsync();
+    }
+}
